dc3036a17829d872e262b44b3496cf144483df36
chrissiewasabi <christinewasabi@gmail.com>
2018-07-17 16:55:07 +0300

    generate data class file

---
+++ app/src/main/java/xyz/megundo/aura/data/ApiResults.kt
@@ -0,0 +1,11 @@
+package xyz.megundo.aura.data
+
+data class ApiResults(
+        val name: String,
+        val description: String,
+        val Rating: String,
+        val episode: Int,
+        val categorie: String,
+        val studio: String,
+        val img: String
+)
