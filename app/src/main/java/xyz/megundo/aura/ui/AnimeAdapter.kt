99df61058feb70d0a8b747f254608b40e5b2ee75
chrissiewasabi <christinewasabi@gmail.com>
2018-07-17 17:45:07 +0300

    list items layout file

--- app/src/main/java/xyz/megundo/aura/ui/AnimeAdapter.kt
+++ app/src/main/java/xyz/megundo/aura/ui/AnimeAdapter.kt
@@ -12,17 +12,17 @@ import xyz.megundo.aura.data.ApiResults
 /*
 based on the default Adapter provided on Android documentation
  */
-class AnimeAdapter (private val myDataset: List<ApiResults>) :
+class AnimeAdapter(private val myDataset: List<ApiResults>) :
         RecyclerView.Adapter<AnimeAdapter.ViewHolder>() {
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var animeName: TextView =itemView.findViewById<TextView>(R.id.tv_anime_name)
-        var animeEpisode: TextView =itemView.findViewById<TextView>(R.id.tv_episode_number)
-        var animeImage: ImageView =itemView.findViewById<ImageView>(R.id.iv_avatar)
+        var animeName: TextView = itemView.findViewById<TextView>(R.id.tv_anime_name)
+        var animeEpisode: TextView = itemView.findViewById<TextView>(R.id.tv_episode_number)
+        var animeImage: ImageView = itemView.findViewById<ImageView>(R.id.iv_avatar)
         fun bind(item: ApiResults) = with(itemView) {
             animeName.text = item.name
-            animeEpisode.text="Episode: "+item.episode
+            animeEpisode.text = "Episode: " + item.episode
 
             //Todo use glide to load image
 
